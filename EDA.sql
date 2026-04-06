686adbf398040ea8bf473cd86cb4a6aa1d72c166
mohammedaburahma <morahma863@gmail.com>
2026-04-06 18:39:17 +0300

    time series analysis and ranking

--- EDA.sql
+++ EDA.sql
@@ -36,3 +36,56 @@ SELECT YEAR(`date`) AS yearly, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY yearly
 ORDER BY 1 DESC;
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+SELECT company, AVG(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `month`
+ORDER BY 1 ASC;
+
+WITH Rolling_total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2 
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `month`
+ORDER BY 1 ASC
+)
+SELECT `month`, total_off,
+SUM(total_off) OVER(ORDER BY `month`) AS rolling_total
+FROM Rolling_total;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company,YEAR(`date`) , SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH company_year (company,years , total_laid_off) AS 
+(
+SELECT company,YEAR(`date`) , SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), company_year_rank AS
+(
+SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+FROM company_year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM company_year_rank
+WHERE ranking <= 5;
